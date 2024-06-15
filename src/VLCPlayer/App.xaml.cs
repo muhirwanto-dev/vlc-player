7c68008c02039ea1e90d4900f55a483d49cef19a
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2024-06-15 08:38:42 +0800

    init: add initial project solution

---
+++ src/VLCPlayer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VLCPlayer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/VLCPlayer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VLCPlayer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
